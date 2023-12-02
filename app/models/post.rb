25c6dc9e3957b3c62cbe891b8ad920095e16691f
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-02 14:12:52 +0200

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_save :post_counter
+
+  private
+
+  def post_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
