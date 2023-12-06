7f0de62bb9c98c5051b441e6d11b6cd2ec0fef67
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-06 16:15:19 +0200

    Test posts controller

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe PostsController, type: :controller do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get :index, params: { user_id: 1 }
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      get :index, params: { user_id: 1 }
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      get :show, params: { user_id: 1, id: 1 }
+      expect(response).to be_successful
+    end
+
+    it 'renders the show template' do
+      get :show, params: { user_id: 1, id: 1 }
+      expect(response).to render_template(:show)
+    end
+  end
+end
