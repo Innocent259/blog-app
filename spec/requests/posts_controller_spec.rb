9440a4eba882af5a1e2e6f37b9adf2ed60feb025
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-07 11:51:04 +0200

    Update posts controller tests

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,27 +1,35 @@
-require 'rails_helper'
-
-describe PostsController, type: :controller do
-  describe 'GET #index' do
-    it 'returns a successful response' do
-      get :index, params: { user_id: 1 }
-      expect(response).to be_successful
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns a success response' do
+      get '/users/:id/posts'
+      expect(response). to be_successful
     end
 
-    it 'renders the index template' do
-      get :index, params: { user_id: 1 }
+    it 'renders the correct template' do
+      get '/users/:id/posts'
       expect(response).to render_template(:index)
+    end
+
+    it 'should include the placeholder' do
+      get '/users/:id/posts'
+      expect(response.body).to include('User posts')
     end
   end
 
-  describe 'GET #show' do
+  describe 'GET /show' do
     it 'returns a successful response' do
-      get :show, params: { user_id: 1, id: 1 }
+      get '/users/:id/posts/:id'
       expect(response).to be_successful
     end
 
     it 'renders the show template' do
-      get :show, params: { user_id: 1, id: 1 }
-      expect(response).to render_template(:show)
+      get '/users/:id/posts/:id'
+      expect(response).to render_template('posts/show')
+    end
+
+    it 'should include the placeholder' do
+      get '/users/:id/posts/:id'
+      expect(response.body).to include('Single post')
     end
   end
 end
