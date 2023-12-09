fc27452b614c9581a8f431e0b78b76abac79bcaa
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-10 00:06:25 +0200

    Update Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,26 @@ class PostsController < ApplicationController
   def index
     @users = [User.find(params[:user_id])]
     @posts = @users[0].posts
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render 'new'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 
   def show
