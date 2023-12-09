2545516c3c99f07c5e5e404e7c5ff84603c4c7e3
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-10 00:04:56 +0200

    Create Likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+    def create
+        @post = Post.find(params[:post_id])
+        @like = Like.new(user: current_user, post: @post)
+    
+        if @like.save
+          redirect_to user_post_path(@post.author, @post), notice: 'Liked the post!'
+        else
+          redirect_to user_post_path(@post.author, @post), alert: 'Unable to like the post.'
+        end
+    end
+end
