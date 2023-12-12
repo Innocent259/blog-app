9afda10691669d7cb32b1ec1de06bff3fa5cf99c
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-12 18:52:11 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] 
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:index, :show, :new, :create]
+      resources :likes, only: [:index, :show, :new, :create], controller: 'post_likes'
+    end
   end
 end
