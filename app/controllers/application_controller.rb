42fbbaccf7acc12ae60ed4931bca6fee6cfd091b
mathsoft29 <iwuconcept@gmail.com>
2023-12-22 10:39:33 +0100

    Implement the devise filter and model configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio email photo password confirm_password])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+	end
 end
