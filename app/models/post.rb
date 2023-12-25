0fb0951e34496d153036f67550bcaaa024f920ae
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-25 20:18:55 +0200

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,22 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter
+  after_destroy :decrease_user_posts_counter
+  after_initialize :set_default
 
   def update_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrease_user_posts_counter
+    author.decrement!(:posts_counter)
   end
 
   def recent_five_comments
