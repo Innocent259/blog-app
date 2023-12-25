425690cd8b8e8165a56d5ceb2f9a404a5db3460f
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-12-25 20:20:38 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create ] do
-      resources :comments, only: [:create, :new]
+    resources :posts, only: [:index, :show, :new, :create, :destroy ] do
+      resources :comments, only: [:create, :new, :destroy]
        resources :likes, only: [:create, :new] 
     end
   end
